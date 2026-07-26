30e76e7f9d14328fa9e26b3451168c303a5bfe64
Muktinath Rajbanshi <muktinathrajbanshi786@gmail.com>
2026-07-26 17:53:33 +0545

    now let's focus on another tutorial in Python

---
+++ main.py
@@ -0,0 +1,40 @@
+# message = "Hello World"
+# message = 'bobby\'s World'
+# # message = "bobby's World"
+# message = """bobby's World was a good
+# cartoon in the 1990s"""
+from email import message
+
+# message = "Hello World"
+
+# print(len(message))
+# print(message[0:5])
+# print(message[:5])
+# print(message[6:])
+
+# print(message)
+
+# message = "Hello World"
+# print(message.lower())
+# print(message.upper())
+# print(message.count("Hello"))
+# print(message.count("l"))
+# print(message.find("World"))
+# print(message.find("universe"))
+
+# message = "Hello World"
+# message = message.replace("World", "Universe")
+# print(message)
+
+greeting = "Hello"
+name = "Michael"
+
+# message = greeting + ", " + name
+# message = greeting + ", " + name + ". Welcome!"
+# message = "{}, {}. Welcome!".format(greeting, name)
+
+# message = f"{greeting}, {name.upper()}. Welcome!"
+
+# print(dir(name))
+# print(help(str))
+print(help(str.lower))
