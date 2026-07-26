8cc1b88f33b2e9fe96401c9d4b8b843b5a433096
Muktinath Rajbanshi <muktinathrajbanshi786@gmail.com>
2026-07-26 18:21:44 +0545

    Integers and Floats in Python-Working with Numeric Data

--- main.py
+++ main.py
@@ -37,4 +37,80 @@ name = "Michael"
 
 # print(dir(name))
 # print(help(str))
-print(help(str.lower))
+# print(help(str.lower))
+
+# Integers and Floats
+# num = 3
+# # num = 3.4
+# print(type(num))
+
+# Arithmetic Operators:
+# Addition: 3 + 2
+# Subtraction: 3 - 2
+# Multiplication: 3 * 2
+# Division: 3 / 2
+# Floor Division: 3 // 2
+# Exponent: 3 ** 2
+# Modulus: 3 % 2
+
+# print(3 * (2 + 1))
+#
+# num = 1
+# num *= 10
+# print(num)
+
+# print(abs(-3))
+# print(round(3.75, 1))
+
+# Comparisons:
+# Equal: 3 == 2
+# num_1 = 3
+# num_2 = 2
+#
+# print(num_1 == num_2)
+
+# Not Equal: 3 != 2
+# num_1 = 3
+# num_2 = 2
+#
+# print(num_1 != num_2)
+# Greater Than: 3 > 2
+# num_1 = 3
+# num_2 = 2
+#
+# print(num_1 > num_2)
+# Less Than: 3 < 2
+# num_1 = 3
+# num_2 = 2
+#
+# print(num_1 < num_2)
+# Greater or Equal: 3 >= 2
+# num_1 = 3
+# num_2 = 2
+#
+# print(num_1 >= num_2)
+# Less or Equal: 3 <= 2
+# num_1 = 3
+# num_2 = 2
+#
+# print(num_1 <= num_2)
+
+num_1 = "100"
+num_2 = "200"
+
+num_1 = int(num_1)
+num_2 = int(num_2)
+
+print((num_1 + num_2))
+
+
+
+
+
+
+
+
+
+
+
+
