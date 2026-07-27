f0b5128074b745cfbf55eff1d585320effdbac4a
Muktinath Rajbanshi <muktinathrajbanshi786@gmail.com>
2026-07-27 11:11:45 +0545

    hait yar set also completed

--- main.py
+++ main.py
@@ -179,17 +179,24 @@ name = "Michael"
 
 # Tuples
 # Immutable
-tuple_1 = ["History", "Math", "Physics", "CompSci"]
-tuple_2 = tuple_1
+# tuple_1 = ["History", "Math", "Physics", "CompSci"]
+# tuple_2 = tuple_1
+#
+# print(tuple_1)
+# print(tuple_2)
+#
+# tuple_1[0] = "Art"
+#
+# print(tuple_1)
+# print(tuple_2)
 
-print(tuple_1)
-print(tuple_2)
+# Sets
+cs_courses = {"History", "Math", "Physics", "CompSci", "Math"}
+art_courses = {"History", "Math", "Art", "Design"}
 
-tuple_1[0] = "Art"
-
-print(tuple_1)
-print(tuple_2)
-
+# print(cs_courses.intersection(art_courses))
+# print(cs_courses.difference(art_courses))
+print(cs_courses.union(art_courses))
 
 
 
