080de6362dc66c4bcf2064415cb6d18101a2241a
Muktinath Rajbanshi <muktinathrajbanshi786@gmail.com>
2026-07-28 16:07:16 +0545

    Lists, Tuples, and Sets completed in Python

--- main.py
+++ main.py
@@ -191,15 +191,24 @@ name = "Michael"
 # print(tuple_2)
 
 # Sets
-cs_courses = {"History", "Math", "Physics", "CompSci", "Math"}
-art_courses = {"History", "Math", "Art", "Design"}
+# cs_courses = {"History", "Math", "Physics", "CompSci", "Math"}
+# art_courses = {"History", "Math", "Art", "Design"}
 
 # print(cs_courses.intersection(art_courses))
 # print(cs_courses.difference(art_courses))
-print(cs_courses.union(art_courses))
+# print(cs_courses.union(art_courses))
 
+# Empty Lists
+empty_list = []
+empty_list = list()
 
+# Empty Tuples
+empty_tuple = ()
+empty_tuple = tuple()
 
+# Empty Sets
+empty_set = {} # This isn't right! It's a dict
+empty_set = set()
 
 
 
