e09bc64f4110e7edc3511f7190d7525af40845bb
Muktinath Rajbanshi <muktinathrajbanshi786@gmail.com>
2026-07-30 11:39:53 +0545

    Loops and Iterations in Python completed - For/While Loops

--- main.py
+++ main.py
@@ -340,11 +340,19 @@ name = "Michael"
 #     print(x)
 #     x += 1
 
+#
+# x = 0
+# while x < 10:
+#     if x == 5:
+#         break
+#     print(x)
+#     x += 1
+
 
 x = 0
-while x < 10:
-    if x == 5:
-        break
+while True:
+    # if x == 5:
+    #     break
     print(x)
     x += 1
 
