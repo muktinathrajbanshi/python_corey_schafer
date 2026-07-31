c0cab158b21f392a12e366bca6958359c4b73549
Muktinath Rajbanshi <muktinathrajbanshi786@gmail.com>
2026-07-31 13:19:51 +0545

    wow this one also good approach

--- intro.py
+++ intro.py
@@ -1,10 +1,21 @@
 # import my_module as mm
+# from my_module import find_index as fi, test
+# from my_module import *
+# import sys
+
+import sys
+sys.path.append("/Users/ACER/Desktop")
+
 from my_module import find_index, test
+
 
 courses = ["History", "Math", "Physics", "Chemistry"]
 
 # index = mm.find_index(courses, "Math")
 index = find_index(courses, "Math")
+# index = fi(courses, "Math")
 
-print(index)
-print(test)
+# print(index)
+# print(test)
+
+print(sys.path)
