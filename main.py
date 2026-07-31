53ab711ab25c26c6f7824590eff7531d1457b563
Muktinath Rajbanshi <muktinathrajbanshi786@gmail.com>
2026-07-31 10:46:40 +0545

    wow function in python is completed

--- main.py
+++ main.py
@@ -409,12 +409,35 @@ name = "Michael"
 # student_info(courses, info)
 
 
-def student_info(*args, **kwargs):
-    print(args)
-    print(kwargs)
+# def student_info(*args, **kwargs):
+#     print(args)
+#     print(kwargs)
 
-courses = ["Math", "Art"]
-info = {"name": "Muktinath", "age": 25}
+# courses = ["Math", "Art"]
+# info = {"name": "Muktinath", "age": 25}
 
-student_info(*courses, **info)
+# student_info(*courses, **info)
 
+# Number of days per month. First value placeholder for indexing purposes.
+month_days = [0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
+
+def is_leap(year):
+    # Return True for leap years, False for non-leap years.
+
+    return year % 4 == 0 and (year % 100 != 0 or year % 400 == 0)
+
+def days_in_month(year, month):
+    # Return number of days in that month in that year.
+
+    # year 2017
+    # month 2 
+    if not 1 <= month <= 12:
+        return "Invalid month"
+
+    if month == 2 and is_leap(year):
+        return 29
+
+    return month_days[month]
+
+# print(is_leap(2020))
+print(days_in_month(2017, 2))
