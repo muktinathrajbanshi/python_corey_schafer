828f490f29a8fdfde92001737543d64f10056ffd
Muktinath Rajbanshi <muktinathrajbanshi786@gmail.com>
2026-07-31 13:39:12 +0545

    wow it's an briliant idea

--- intro.py
+++ intro.py
@@ -3,19 +3,27 @@
 # from my_module import *
 # import sys
 
-import sys
-sys.path.append("/Users/ACER/Desktop")
+# import sys
+# sys.path.append("/Users/ACER/Desktop")
 
-from my_module import find_index, test
+# from my_module import find_index, test
 
 
-courses = ["History", "Math", "Physics", "Chemistry"]
+# courses = ["History", "Math", "Physics", "Chemistry"]
 
 # index = mm.find_index(courses, "Math")
-index = find_index(courses, "Math")
+# index = find_index(courses, "Math")
 # index = fi(courses, "Math")
 
 # print(index)
 # print(test)
 
-print(sys.path)
+# print(sys.path)
+
+# import random
+#
+# courses = ["History", "Math", "Physics", "Chemistry"]
+#
+# random_course = random.choice(courses)
+#
+# print(random_course)
